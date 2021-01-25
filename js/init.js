29bba1fff593134deb7b1f78ea0c948dd13c8402
jorgam8 <76073257+jorgam8@users.noreply.github.com>
2021-01-24 18:33:33 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,8 +1,9 @@
 firebase.initializeApp({
-    apiKey: "AIzaSyD-NCeU2mHd8OnAuyYVmV4UW-F5pYf9j1Q",
-    authDomain: "redsocial-5e2f9.firebaseapp.com",
-    projectId: "redsocial-5e2f9",
-    storageBucket: "redsocial-5e2f9.appspot.com",
-    messagingSenderId: "1056596275656",
-    appId: "1:1056596275656:web:e9f79a1e60178c3eb890fd"
+    apiKey: "AIzaSyDruRK5q62al_-rS6C2P3pFm4p-1BNPhi4",
+    authDomain: "miniredsocial-56420.firebaseapp.com",
+    projectId: "miniredsocial-56420",
+    storageBucket: "miniredsocial-56420.appspot.com",
+    messagingSenderId: "479483773412",
+    appId: "1:479483773412:web:58b4e98d03c38c5d7a87f1",
+    measurementId: "G-JXPS50ZF3Z"
 });
